5859886c4121e7511a0c3e496567e58ec75a9732
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-06 20:58:59 -0600

    Update navbar

---
+++ app/route/application.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <div class="">
+    <div class="top-nav">
+      <div class="container">
+        <nav class="navbar">
+          <h1 class="navbar__logo">Library</h1>
+
+          <div class="navbar__center"></div>
+
+          <div class="navbar__right">
+            <ul class="nav-list">
+              <li class="nav-list__item">All Books</li>
+              <li class="nav-list__item">Add Book</li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+
+    <div class="section">
+      <div class="container">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
